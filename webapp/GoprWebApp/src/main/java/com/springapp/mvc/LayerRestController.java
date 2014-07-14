61217236006ab63dd7e86a81c4907d3afcf9eb2b
Ucash <ucashek@gmail.com>
2014-07-14 21:35:16 +0200

    webapp

--- webapp/GoprWebApp/src/main/java/com/springapp/mvc/LayerRestController.java
+++ webapp/GoprWebApp/src/main/java/com/springapp/mvc/LayerRestController.java
@@ -5,6 +5,7 @@ import com.esri.core.map.Graphic;
 import com.esri.map.GraphicsLayer;
 import com.esri.map.JMap;
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,8 @@ public class LayerRestController {
 
     public static final String GET_LAYER = "/rest/layer/get";
     public static final String SEND_LAYER = "/rest/layer/send";
+    public static final String GET_POINTS = "/rest/point/get";
+    public static final String SEND_POINTS = "/rest/point/send";
 
     /*
      * Returns searching areas connected with given action.
@@ -37,7 +40,7 @@ public class LayerRestController {
      * Save searching areas of given action.
      *
      * Areas giving in structures:
-     * JSONObject geometries = Map<"geometries", Array<Area>>
+     * JSONObject.geometries = Map<"geometries", Array<Area>>
      *
      * Area = areaId (this is index of grphic in webapp map),
      *        area (this is geometry.toJson() stringify)
@@ -47,6 +50,60 @@ public class LayerRestController {
     @ResponseBody
     boolean receiveGeometries(@RequestParam("actionId") long actionId, @RequestParam("geometries") JSONObject geometries) {
         logger.info("SEND LEYER: action: " + actionId + ", pola:\n\t" + geometries);
+
+        //TODO: save jsonobject["geometries"] to db )
+
+        return true;
+    }
+
+    @RequestMapping(value = GET_POINTS, method = RequestMethod.GET)
+    public
+    @ResponseBody
+    String passPoints(@RequestParam("actionId") int actionId) {
+        logger.info("GET LAYER: " + actionId);
+
+        JSONObject json1 = new JSONObject();
+        JSONArray jsonArray1 = new JSONArray();
+        JSONObject coordinate1 = new JSONObject();
+        coordinate1.put("x", -9664114.480484284);
+        coordinate1.put("y", 3962469.970217699);
+        jsonArray1.put(coordinate1);
+        json1.put("userId", "1");
+        json1.put("postions", jsonArray1);
+
+        JSONObject json2 = new JSONObject();
+        JSONArray jsonArray2 = new JSONArray();
+        JSONObject coordinate2 = new JSONObject();
+        coordinate2.put("x", -9664114.480484284);
+        coordinate2.put("y", 3962469.970217699);
+        JSONObject coordinate3 = new JSONObject();
+        coordinate3.put("x", 2214749.0606268025);
+        coordinate3.put("y", 6460923.093105682);
+        jsonArray2.put(coordinate2);
+        jsonArray2.put(coordinate3);
+        json2.put("userId", "2");
+        json2.put("postions", jsonArray2);
+
+        JSONObject json3 = new JSONObject();
+        JSONArray jsonArray3 = new JSONArray();
+        json3.put("userId", "3");
+        json3.put("postions", jsonArray3);
+
+        JSONArray result = new JSONArray();
+        result.put(json1);
+        result.put(json2);
+        result.put(json3);
+
+        logger.info(result.toString());
+
+        return result.toString();
+    }
+
+    @RequestMapping(value = SEND_POINTS, method = RequestMethod.POST)
+    public
+    @ResponseBody
+    boolean receivePoints(@RequestParam("actionId") long actionId, @RequestParam("positions") JSONObject positions) {
+        logger.info("SEND LEYER: action: " + actionId + ", pola:\n\t" + positions.toString());
 
         //TODO: save jsonobject["geometries"] to db )
 
